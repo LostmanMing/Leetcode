7f62f077b58cb7f37b33f3272cda9a3a9d4e6f14
gming zhao <865301453@qq.com>
2022-12-18 15:03:40 +0800

    add pause

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,8 @@
 #include "iostream"
 
 int main(){
-    std::cout<<"Hello World!"<<std::endl;
-    std::cout<<"你好世界！"<<std::endl;
-    
+    std::cout<<"Hello World!"<<std::endl; 
+    std::cout<<"你好世界"<<std::endl;
+    system("pause");
     return 0;
 }
