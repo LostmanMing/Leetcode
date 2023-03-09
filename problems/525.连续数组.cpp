746f76bfbd85542bb848c3e2ee401d2580e9faec
zhaogming <865301453@qq.com>
2023-03-09 20:53:58 +0800

    add problem 525

--- problems/525.连续数组.cpp
+++ problems/525.连续数组.cpp
@@ -12,16 +12,16 @@ using namespace std;
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        mp[0] = -1;
+        unordered_map<int,int> mp;              //记录前缀和第一次出现的下标
+        mp[0] = -1;                             //
         int n = nums.size();
         int sum = 0, res = 0;
         for(int i = 0;i < n;i++){
             sum += (nums[i]==0 ? -1:1);
-            if(mp.count(sum)){
-                res = max(res, i - mp[sum]);
+            if(mp.count(sum)){                  //如果当前前缀和sum已经在哈希表中出现过了
+                res = max(res, i - mp[sum]);    //更新最长子数组长度
             }else{
-                mp[sum] = i;
+                mp[sum] = i;    
             }
         }
         return res;
