4197522fcb4d828a8663c6ae6aaafcd9e34d98b5
gming zhao <865301453@qq.com>
2023-07-31 19:34:13 +0800

    snd_loop 16-20

---
+++ snd_loop/P20 647.回文子串.cpp
@@ -0,0 +1,38 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem647.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=647 lang=cpp
+ *
+ * [647] 回文子串
+ */
+
+// @lc code=start
+#include<string>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int res = 0;
+        for(int i = 0;i < n;i++){
+            for(int l = i,r = i;l >= 0 && r < n;l--,r++){
+                if(s[l] == s[r]) res++;
+                else break;
+            }
+            for(int l = i,r = i + 1;l >= 0 && r < n;l--,r++){
+                if(s[l] == s[r]) res++;
+                else break;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
